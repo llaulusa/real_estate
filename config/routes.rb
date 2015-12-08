23100645ba4f334c3347413184cf372d8b245af1
Ladd Laulusa <laddlaulusa@gmail.com>
2015-12-07 17:07:05 -0700

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'properties/index'
+
+  get 'properties/show'
+
+  get 'properties/new'
+
+  get 'properties/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
